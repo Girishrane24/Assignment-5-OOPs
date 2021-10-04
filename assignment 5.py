4c526132cb9ef86c29876096ba800311723dca0f
Girishrane24 <57502956+Girishrane24@users.noreply.github.com>
2021-10-04 20:22:13 +0530

    Update assignment 5.py

--- assignment 5.py
+++ assignment 5.py
@@ -1,6 +1,10 @@
 class girish:
-    def power(self,x,n):
-        return pow(x,n)
+    def __init__(self,x,n):
+        self.x = x
+        self.n = n
+        self.ans = 0
+    def power(self):
+        return pow(self.x,self.n)
 x,n = map(int,input().split())
-obj1 = girish()
-print(obj1.power(x,n))
+obj1 = girish(x,n)
+print(obj1.power())
